7284889890ef92f86dfb67d3782f8ce1e235c941
0uizi0 <0uizi0@naver.com>
2024-06-10 16:56:29 +0900

    feat: switch문을 활용하지 않은 오늘 날짜의 요일 출력하기

--- 04w/pr3.js
+++ 04w/pr3.js
@@ -28,5 +28,14 @@ const checkDayOfWeek1 = (day) => {
   return `오늘은 ${dayOfWeek}요일 입니다.`;
 };
 
+// switch문을 사용하지 않은 방법
+const checkDayOfWeek2 = (day) => {
+  const WEEK_NAMES = "일월화수목금토";
+  const dayNum = day.getDay(); // 요일번호 (Sunday - Saturday : 0 - 6)
+  const dayOfWeek = WEEK_NAMES[dayNum];
+  return `오늘은 ${dayOfWeek}요일 입니다.`;
+};
+
 const today = new Date();
 console.log(checkDayOfWeek1(today));
+console.log(checkDayOfWeek2(today));
