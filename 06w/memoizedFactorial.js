5a1ae5799c49acc0d25fa097f63f992e88077bdd
0uizi0 <0uizi0@naver.com>
2024-06-17 17:35:32 +0900

    ♻️ Refactor: memoizedFactorial 함수 코드 리팩터링

--- 06w/memoizedFactorial.js
+++ 06w/memoizedFactorial.js
@@ -1,18 +1,11 @@
 const memoized = (fn) => {
   const memoizedTable = {};
-  return (k) => memoizedTable[k] || (memoizedTable[k] = fn(k));
+  return (n) => memoizedTable[n] || (memoizedTable[n] = fn(n));
 };
 
-// let memoizedFactorialRunCnt = 0;
-
 const memoizedFactorial = memoized((n) => {
-  // memoizedFactorialRunCnt += 1;
-  if (n === 1) return 1;
+  if (n <= 1) return 1;
   return n * memoizedFactorial(n - 1);
 });
 
-console.log(memoizedFactorial(3)); // 6
-// console.log(`runCnt = ${memoizedFactorialRunCnt}`); // runCnt = 3
-// memoizedFactorialRunCnt = 0;
-console.log(memoizedFactorial(5)); // 120
-// console.log(`runCnt = ${memoizedFactorialRunCnt}`); // runCnt = 2
+console.log(memoizedFactorial(3));
