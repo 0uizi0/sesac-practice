cff6c230b78a01f8e3eb3fdb000a58cc4350d772
0uizi0 <0uizi0@naver.com>
2024-06-19 16:29:30 +0900

    ✨ Feat: 구조변경 함수 구현

---
+++ 06w/changeStructure.js
@@ -0,0 +1,28 @@
+const testCase = [
+  ["A", 10, 20],
+  ["B", 30, 40],
+  ["C", 50, 60, 70],
+];
+
+const makeObjectFromArray = (arr) => {
+  const obj = {};
+  for (let line of arr) {
+    [k, ...v] = line;
+    obj[k] = v;
+  }
+  return obj;
+};
+const convertedObj = makeObjectFromArray(testCase);
+console.log("🚀 객체로 변환된 결과 :", convertedObj); // { 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] }
+
+// obj => arr
+const makeArrayFromObject = (obj) => {
+  const arr = [];
+  for (let k in obj) {
+    const v = obj[k];
+    arr.push([k, ...v]);
+  }
+  return arr;
+};
+const convertedArr = makeArrayFromObject(convertedObj);
+console.log("🚀 배열로 변환된 결과 :", convertedArr); // [['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]]
