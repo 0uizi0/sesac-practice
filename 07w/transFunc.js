dc7979d0908fcd003f536572d294468d5773dbd2
0uizi0 <0uizi0@naver.com>
2024-06-23 16:03:24 +0900

    ✨ Feat: 값 변환 함수 구현

---
+++ 07w/transFunc.js
@@ -0,0 +1,12 @@
+/* ex1) 배열의 각 원소를 String으로 변환하시오. */
+assert = require("assert");
+const arr = [1, 2, 3, true];
+const ret1 = arr.map((v) => v.toString());
+assert.deepStrictEqual(ret1, ["1", "2", "3", "true"]);
+
+/* ex2) 다음과 같이 작동하는 classNames 함수를 작성하시오. */
+const classNames = (...args) =>
+  args.reduce((acc, cur) => `${acc}${cur == "" ? "" : " "}${cur}`).trim();
+
+const ret2 = classNames("", "a b c", "d", "", "e");
+assert.strictEqual(ret2, "a b c d e");
