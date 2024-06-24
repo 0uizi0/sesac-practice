0724100fce9759f70d3acbdc170326340fea87aa
0uizi0 <0uizi0@naver.com>
2024-06-24 16:03:49 +0900

    ✨ Feat: 수행 순서가 변경 가능한 값 구하기

--- 07w/exeActions.js
+++ 07w/exeActions.js
@@ -14,3 +14,13 @@ const getV = (arr) =>
     return acc;
   }, []);
 assert.deepStrictEqual(getV(testCase), [1, 8, 27, 64, 125]);
+
+/** 수행 순서가 변경 가능한 함수 exeActions */
+const exeActions = (arr, actions) => {
+  return arr.reduce((acc, cur) => {
+    const ret = actions.reduce((v, a) => a(v), cur);
+    acc.push(ret);
+    return acc;
+  }, []);
+};
+assert.deepStrictEqual(exeActions(testCase, orders), [1, 8, 27, 64, 125]);
