853e6a9e5cdc81a3144c133799115f9c54b4c252
0uizi0 <0uizi0@naver.com>
2024-06-24 16:00:11 +0900

    ✨ Feat: 정해진 순서로 계산된 값 구하기

---
+++ 07w/exeActions.js
@@ -0,0 +1,16 @@
+assert = require("assert");
+
+const square = (n) => n ** 2;
+const sqrt = (n) => Math.sqrt(n);
+const cube = (n) => n ** 3;
+const testCase = [1, 2, 3, 4, 5];
+const orders = [square, sqrt, cube];
+
+/** 정해진 순서로 계산된 값을 구하는 함수 getV */
+const getV = (arr) =>
+  arr.reduce((acc, cur) => {
+    const ret = cube(sqrt(square(cur)));
+    acc.push(ret);
+    return acc;
+  }, []);
+assert.deepStrictEqual(getV(testCase), [1, 8, 27, 64, 125]);
