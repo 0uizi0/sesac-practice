c4a37ecda183cdd896b6dffed1d0462687381a9d
0uizi0 <0uizi0@naver.com>
2024-06-24 16:09:34 +0900

    ♻️ Refactor: string 변환 함수 리팩터링

--- 07w/transFunc.js
+++ 07w/transFunc.js
@@ -1,7 +1,7 @@
 /* ex1) 배열의 각 원소를 String으로 변환하시오. */
 assert = require("assert");
 const arr = [1, 2, 3, true];
-const ret1 = arr.map((v) => v.toString());
+const ret1 = arr.map(String);
 assert.deepStrictEqual(ret1, ["1", "2", "3", "true"]);
 
 /* ex2) 다음과 같이 작동하는 classNames 함수를 작성하시오. */
