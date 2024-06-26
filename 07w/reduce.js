3440daffc769340046619a47dd32a4de1457c9e1
0uizi0 <0uizi0@naver.com>
2024-06-27 00:17:48 +0900

    🐛 Fix: reduce 함수의 인자에 객체가 들어올 경우 고려
    객체의 id 값을 더하는 함수의 경우에도 id 값이 string으로 변경되어 에러 발생. 해결 필요

--- 07w/reduce.js
+++ 07w/reduce.js
@@ -1,25 +1,14 @@
 assert = require("assert");
 
-function checkOP(fn) {
-  const formula = fn.toString();
-  if (formula.includes("+")) return "+";
-  if (formula.includes("-")) return "-";
-  if (formula.includes("*")) return "*";
-  if (formula.includes("/")) return "/";
-}
-
-function reduce(arr, fn, initValue) {
-  const op = checkOP(fn);
-  if (initValue == undefined) {
-    initValue = op == "+" || op == "-" ? 0 : 1;
+// TODO: 주석(object - id 테스트케이스) 해결 필요
+function reduce(arr, fn, initValue, i = 0) {
+  let acc =
+    initValue ??
+    ((i = 1), typeof arr[0] === "number" ? arr[0] : fn("", arr[0]));
+  for (; i < arr.length; i += 1) {
+    acc = fn(acc, arr[i]);
   }
-  for (let a of arr) {
-    if (op == "+") initValue += a;
-    if (op == "-") initValue -= a;
-    if (op == "*") initValue *= a;
-    if (op == "/") initValue /= a;
-  }
-  return initValue;
+  return acc;
 }
 
 assert.deepStrictEqual(
@@ -42,3 +31,19 @@ assert.deepStrictEqual(
   reduce([3, 3, 3], (a, b) => a * b, 0),
   0
 );
+
+/** 도전중인 테스트케이스 */
+const users = [
+  { id: 1, name: "Hong" },
+  { id: 20, name: "Kim" },
+  { id: 3, name: "Lee" },
+];
+
+assert.deepStrictEqual(
+  reduce(users, (acc, user) => acc + user.name),
+  "HongKimLee"
+);
+// assert.deepStrictEqual(
+//   reduce(users, (acc, user) => acc + user.id),
+//   24
+// );
