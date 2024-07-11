5521d7bde7a3a4018654556be2408f985c20357e
0uizi0 <0uizi0@naver.com>
2024-07-11 17:02:33 +0900

    ✨ Feat: promiseAll 함수 직접 구현

---
+++ 09w/Promise/promiseAll.js
@@ -0,0 +1,38 @@
+assert = require("assert");
+
+/** promiseAll 함수 직접 구현 */
+const promiseAll = (arr) => {
+  return new Promise((resolve, reject) => {
+    const ret = [];
+    let completed = 0;
+
+    arr.forEach((a, i) => {
+      a.then((res) => {
+        ret[i] = res;
+        if (++completed === arr.length) resolve(ret);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  });
+};
+
+// TEST
+const vals = [1, 2, 3];
+const randTime = (val) =>
+  new Promise((resolve) => setTimeout(resolve, Math.random() * 1000, val));
+
+promiseAll([randTime(1), randTime(2), randTime(3)])
+  .then((arr) => {
+    console.table(arr);
+    assert.deepStrictEqual(arr, vals);
+  })
+  .catch(console.error);
+
+promiseAll([randTime(11), Promise.reject("RRR"), randTime(33)])
+  .then((array) => {
+    console.log("여긴 과연 호출될까?!");
+  })
+  .catch((error) => {
+    console.log("reject!!!!!!>>", error);
+  });
