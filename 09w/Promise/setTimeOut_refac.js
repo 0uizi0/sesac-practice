b27d92708d89be010e166cafaee58424b8b266ef
0uizi0 <0uizi0@naver.com>
2024-07-11 19:04:53 +0900

    ♻️ Refactor: depthTimer 함수 리팩터링

--- 09w/Promise/setTimeOut_refac.js
+++ 09w/Promise/setTimeOut_refac.js
@@ -1,11 +1,9 @@
 /** setTimeOut 코드를 Promise를 이용하여 refactoring */
 const depthTimer = (id) =>
   new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log(`depth${id}`, new Date());
-      if (id < 3) resolve(id + 1);
-      else reject(new Error(`Already ${id}-depth!!`));
-    }, 1000 * id);
+    console.log(`depth${id}`, new Date());
+    if (id >= 3) reject(new Error(`Already ${id}-depth!!`));
+    setTimeout(resolve, 1000 * id, (id += 1));
   });
 
 // TEST
