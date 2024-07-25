c69250f58a0246b4883d7f46cfe0872d625ce07c
0uizi0 <0uizi0@naver.com>
2024-07-25 15:36:59 +0900

    ✨ Feat: 제네릭을 활용해 타입 에러 잡기

---
+++ 11w/generic/stock.ts
@@ -0,0 +1,20 @@
+// 다음 코드가 오류가 나지 않도록 수정하시오.
+// 단, itemPrices의 item에는 재고(stock)에 있는 item들만 가능합니다.
+
+type Item = { item: string; price: number };
+type ItemPrice<T, U> = {
+  [K in keyof T]: keyof U extends T[K] ? keyof U : T[K];
+};
+
+const stock = { X: 1, Y: 2, Z: 30 };
+
+const itemPrices: ItemPrice<Item, typeof stock>[] = [
+  { item: "X", price: 1000 },
+  { item: "Y", price: 2000 },
+  { item: "Z", price: 3000 },
+];
+
+const total = itemPrices.reduce(
+  (curr, itemPrice) => curr + stock[itemPrice.item] * itemPrice.price,
+  0
+);
