545ba6b32f507dc410b1108dcc9842ae641c72e7
0uizi0 <0uizi0@naver.com>
2024-07-27 14:19:56 +0900

    ✨ Feat: Parameters를 활용한 regist 함수 구현

---
+++ 11w/utility_type/regist.ts
@@ -0,0 +1,11 @@
+// regist 함수가 다음과 같을 때 파라미터 처리를 해보세요.
+function registUserObj({ name, age }: { name: string; age: number }) {
+  const id = 100;
+  return { id, name, age };
+}
+
+type RegistUserObj = Parameters<typeof registUserObj>[0];
+
+const paramObj: RegistUserObj = { name: "Hong", age: 32 };
+const newUser2 = registUserObj(paramObj);
+console.log("🚀  newUser2:", newUser2);
