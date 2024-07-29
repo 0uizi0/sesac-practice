2a4653a9bf620ee5440cef2923d0897e5dc5eeb1
0uizi0 <0uizi0@naver.com>
2024-07-29 15:01:31 +0900

    ✨ Feat: 특정 함수의 인자 타입을 추출하는 Args 타입 구현

--- 11w/type_operation/infer.ts
+++ 11w/type_operation/infer.ts
@@ -1,7 +1,10 @@
 // 특정 함수의 인자 타입을 추출하는 유틸리티 타입을 작성하시오. (infer)
-type FirstArgs<F> = F extends (...args: infer Arg) => unknown ? Arg[0] : never;
-type SecondArgs<F> = F extends (...args: infer Arg) => unknown ? Arg[1] : never;
-// type Args<F> = F extends (...args: infer Arg) => unknown ? Arg : never;
+type FirstArgs<F> = F extends (...args: infer A) => unknown ? A[0] : never;
+type SecondArgs<F> = F extends (...args: infer A) => unknown ? A[1] : never;
+type Args<F> = F extends (...args: infer A) => unknown ? ArrayItems<A> : never;
+
+// 배열 타입의 원소 타입 추출
+type ArrayItems<T> = T extends (infer U)[] ? U : never
 
 function add(a: number, b: string) {
   return `${a} - ${b}`;
@@ -9,9 +12,9 @@ function add(a: number, b: string) {
 
 type A = FirstArgs<typeof add>; // number
 type B = SecondArgs<typeof add>; // string
-// type C = Args<typeof add>; // number | string
+type C = Args<typeof add>; // number | string
 
-// type AX = Args<typeof String.prototype.endsWith>;
-// // ⇒ string | number | undefined
-// type AX = Args<typeof String.prototype.charAt>;
-// // ⇒ number
+type AX = Args<typeof String.prototype.endsWith>;
+// ⇒ string | number | undefined
+type AXX = Args<typeof String.prototype.charAt>;
+// ⇒ number
