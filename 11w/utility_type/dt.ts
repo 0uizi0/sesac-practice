1acbb8ee661a06552e69d8e46aadba775705610c
0uizi0 <0uizi0@naver.com>
2024-07-29 15:48:41 +0900

    🐛 Fix: 타입스크립트로 디바운스, 쓰로틀 함수 구현
    any가 최선이라고 생각하는데, 확인 필요

--- 11w/utility_type/dt.ts
+++ 11w/utility_type/dt.ts
@@ -1,23 +1,23 @@
 // debounce와 throttle 함수를 TypeScript로 작성하시오.
-function debounce(cb: (prop: number) => void, delay: number) {
+function debounce<F extends (...args: any[]) => any>(cb: F, delay: number) {
   let timer: NodeJS.Timeout | null;
 
-  return (props: Parameters<typeof cb>[0]) => {
+  return (...args: Parameters<F>) => {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
-      cb(props);
+      cb(...args);
       timer = null;
     }, delay);
   };
 }
 
-function throttle(cb: (prop: number) => void, delay: number) {
+function throttle<F extends (...args: any[]) => any>(cb: F, delay: number) {
   let timer: NodeJS.Timeout | null;
 
-  return (props: Parameters<typeof cb>[0]) => {
+  return (...args: Parameters<typeof cb>) => {
     if (timer) return;
     timer = setTimeout(() => {
-      cb(props);
+      cb(...args);
       timer = null;
     }, delay);
   };
