5bf2b6f541ca08fa72abffcb0845c62312ead251
0uizi0 <0uizi0@naver.com>
2024-07-30 21:12:13 +0900

    ♻️ Refactor: 특정 함수의 인자 타입을 추출하는 Args 타입 구현

--- 11w/type_operation/infer.ts
+++ 11w/type_operation/infer.ts
@@ -1,10 +1,7 @@
 // 특정 함수의 인자 타입을 추출하는 유틸리티 타입을 작성하시오. (infer)
 type FirstArgs<F> = F extends (...args: infer A) => unknown ? A[0] : never;
 type SecondArgs<F> = F extends (...args: infer A) => unknown ? A[1] : never;
-type Args<F> = F extends (...args: infer A) => unknown ? ArrayItems<A> : never;
-
-// 배열 타입의 원소 타입 추출
-type ArrayItems<T> = T extends (infer U)[] ? U : never
+type Args<F> = F extends (...args: infer A) => unknown ? A[number] : never;
 
 function add(a: number, b: string) {
   return `${a} - ${b}`;
