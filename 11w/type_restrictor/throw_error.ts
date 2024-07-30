b814473a40945b05bab84b6899e58fd93885df70
0uizi0 <0uizi0@naver.com>
2024-07-30 20:28:15 +0900

    🐛 Fix: instanceof를 활용한 에러 메시지 출력하기

--- 11w/type_restrictor/throw_error.ts
+++ 11w/type_restrictor/throw_error.ts
@@ -1,16 +1,16 @@
 // 다음에서 '가', '나', '다' 어떤 걸 throw 해도 에러 메시지를 출력하도록 (라) 부분을 수정하시오.
+const isMessageError = (
+  err: unknown
+): err is {
+  message: string;
+} => !!err && typeof err === "object" && "message" in err;
 
 try {
-  const newLocal = "error";
-  // throw new Error("some error!!!!"); // 가
-  // throw 'some string error!!!';        // 나
-  throw ["some", "array", newLocal]; // 다
+  // throw new AError('AError Occurs!!');
+  // throw new Error('some error!!!!'); // 가
+  // throw 'some string error!!!'; // 나
+  throw ["some", "array", "error"]; // 다
 } catch (error) {
-  const message =
-    error instanceof Error
-      ? error.message
-      : Array.isArray(error)
-      ? error.join(" ")
-      : error;
-  console.log(JSON.stringify(message));
+  if (isMessageError(error)) console.log(error.message); // (라)
+  else console.log(JSON.stringify(error));
 }
