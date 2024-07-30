bd673ce8dc94a531a57eefb3c0a35821ce91b187
0uizi0 <0uizi0@naver.com>
2024-07-30 20:30:48 +0900

    🐛 Fix: deleteArray 구현

--- 11w/type_restrictor/deleteArray.ts
+++ 11w/type_restrictor/deleteArray.ts
@@ -1,42 +1,39 @@
-import * as assert from "assert";
-
 type TPropertyKeyType = string | number | symbol;
 type TUser = { [key: string]: string | number };
 
-function deleteArray(
-  arr: TUser[] | number[],
-  startOrKey: TPropertyKeyType,
-  endOrValue?: unknown
+function deleteArray<T>(
+  arr: T[],
+  startOrKey: number | keyof T,
+  endOrValue?: number | T[keyof T]
 ) {
   if (typeof startOrKey === "number") {
-    if (typeof endOrValue === "number") {
-      return arr.filter((_, i) => i < startOrKey || i > endOrValue - 1);
-    }
-    return arr.slice(0, startOrKey);
+    return arr.filter(
+      (_, i) =>
+        i < startOrKey ||
+        i >
+          (typeof endOrValue === "number"
+            ? endOrValue - 1
+            : Number.MAX_SAFE_INTEGER)
+    );
   }
 
-  if (typeof startOrKey === "string") {
-    arr.filter((e) => {
-      if (e && typeof e === "object") {
-        // e['id'];  error
-        // e[startOrKey];  error
-      }
-    });
-  }
-
-  // if (typeof startOrKey === 'symbol') {...}
-
-  return [];
+  return arr.filter(
+    (a) => a && typeof a === "object" && a[startOrKey] !== endOrValue
+  );
 }
 
 const arr = [1, 2, 3, 4];
-assert.deepStrictEqual(deleteArray(arr, 2), [1, 2]);
-assert.deepStrictEqual(deleteArray(arr, 1, 3), [1, 4]);
+console.log(deleteArray(arr, 2)); // [1, 2]
+console.log(deleteArray(arr, 1, 3)); // [1, 4]
+console.log(arr); // [1, 2, 3, 4]
 
-const Hong = { id: 1, name: "Hong" };
-const Kim = { id: 2, name: "Kim" };
-const Lee = { id: 3, name: "Lee" };
-const users: TUser[] = [Hong, Kim, Lee];
+const userss = [
+  { id: 1, name: "Hong" },
+  { id: 2, name: "Kim" },
+  { id: 3, name: "Lee" },
+];
 
-assert.deepStrictEqual(deleteArray(users, 2), [Hong, Kim]);
-assert.deepStrictEqual(deleteArray(users, "id", 2), [Hong, Lee]);
+console.log(deleteArray(userss, 2)); // [Hong, Kim]
+console.log(deleteArray(userss, 1, 2)); // [Hong, Lee]
+console.log(deleteArray(userss, "id", 2)); // [Hong, Lee]
+console.log(deleteArray(userss, "name", "Lee")); // [Hong, Kim]
